681cc74caf536ad9b2ac55f2090f6ec1531f868d
Emon <ishrakuzzamanemon201@gmail.com>
2025-02-28 01:27:01 +0100

    Check winner

--- TIC_TAC_TOE.cpp
+++ TIC_TAC_TOE.cpp
@@ -79,5 +79,30 @@ void ComputerMove(char *spaces, char computer){
 
 // Function to check winner
 bool CheckWinner(char *spaces, char player, char computer){
-    return 0;
+    if(spaces[0]!=' ' && (spaces[0] == spaces[1] && spaces[1] == spaces[2])){
+        spaces[0]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    } else if(spaces[3]!=' ' && (spaces[3] == spaces[4] && spaces[5] == spaces[6])){
+        spaces[3]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    } else if(spaces[6]!=' ' && (spaces[6] == spaces[7] && spaces[7] == spaces[8])){
+        spaces[6]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    }
+
+    else if(spaces[0]!=' ' && (spaces[0] == spaces[3] && spaces[3] == spaces[6])){
+        spaces[0]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    } else if(spaces[1]!=' ' && (spaces[1] == spaces[4] && spaces[4] == spaces[7])){
+        spaces[1]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    } else if(spaces[2]!=' ' && (spaces[2] == spaces[5] && spaces[5] == spaces[8])){
+        spaces[2]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    }
+
+    else if(spaces[0]!=' ' && (spaces[0] == spaces[4] && spaces[4] == spaces[8])){
+        spaces[0]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    }
+
+    else if(spaces[2]!=' ' && (spaces[2] == spaces[4] && spaces[4] == spaces[6])){
+        spaces[2]==player? std::cout<<"YOU wIN!\n" : std::cout<<"YOU LOSE!\n";
+    } else{
+        return false;
+    }
+    return true;
 }
